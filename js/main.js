a7e01cef7f9bcb15a4b469c2a0d95b97ddeb4215
zseever <zseever@gmail.com>
2022-07-01 13:17:22 -0700

    didnt save JS file, defined state variables

--- js/main.js
+++ js/main.js
@@ -1,5 +1,26 @@
 /*----- constants -----*/
+
+
 /*----- app's state (variables) -----*/
+// -store arrays from main object in variables
+// -Arrays used for: Deck, Drawn cards, Column piles 1-7,  and the 4 ace piles
+const cardPiles;
+// -variable for game in progress or game won (I’m not sure how to determine a game is “unwinnable” in solitaire)
+let gameStatus;
+// -move counter?
+let moveCounter;
+// -current card/stack selected
+let currentPile;
+// Timer (extra feature)
+let count;
+
+
 /*----- cached element references -----*/
+
+
 /*----- event listeners -----*/
+
+
 /*----- functions -----*/
+
+
